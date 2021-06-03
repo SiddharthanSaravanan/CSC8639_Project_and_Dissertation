90e511b02ebb08736b510aba40f157d4e63d4170
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2021-06-03 16:17:28 +0100

    Exploratory file

--- DataVisualization_Dissertation/src/eda.R
+++ DataVisualization_Dissertation/src/eda.R
@@ -1,8 +1,15 @@
-library('ProjectTemplate')
-load.project()
+#Plotting:
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
+
+
+(plot_1 = ggplot(sample_data_no2, aes(sample_data_no2$date, sample_data_no2$Value, color = sample_data_no2$City)) + geom_line())
+
+(plot_2 = ggplot(sample_data_pm10, aes(sample_data_pm10$date, sample_data_pm10$Value, color = sample_data_pm10$City)) + geom_line())
+
+
+
+
+
+(plot_3 = ggplot(ncl_mcu_no2, aes(datetime, Value, color = City)) + geom_line())
+
+(plot_4 = ggplot(ncl_mcu_pm10, aes(ncl_mcu_pm10$datetime, ncl_mcu_pm10$Value, color = ncl_mcu_pm10$City)) + geom_line())
