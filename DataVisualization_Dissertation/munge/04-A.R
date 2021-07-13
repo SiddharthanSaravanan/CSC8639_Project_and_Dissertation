bbb0106822e2fa99f5ce33f80c924b09a173908b
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2021-07-13 15:56:56 +0100

    Lat long conversion

--- DataVisualization_Dissertation/munge/04-A.R
+++ DataVisualization_Dissertation/munge/04-A.R
@@ -1,23 +1,34 @@
 #conversion of postcodes into lat and long
 
-testing = list(paste0(unique(mcu_agg_1$PostCodes), collapse=","))
+#Mcu data---------
 
-a = list("M1","M1 7EL")
+mcu_lat_lon_data = as.data.frame(unique(pivot_mcu_data$PostCodes))
+names(mcu_lat_lon_data)[1] = "PostCodes"
 
-pc_list <- list(
-  postcodes = c("PR3 0SG", "M45 6GN", "EX165BL"))
+mcu_lat_lon_data = cbind(mcu_lat_lon_data, geocode(mcu_lat_lon_data$PostCodes, output = "latlon"))
 
-post = bulk_postcode_lookup(pc_list)
+names(mcu_lat_lon_data)[2] = "Longitude"
+names(mcu_lat_lon_data)[3] = "Latitude"
+
+mcu_lat_lon_data$Longitude = as.character(mcu_lat_lon_data$Longitude)
+mcu_lat_lon_data$Latitude = as.character(mcu_lat_lon_data$Latitude)
+
+mcu_final_lat_lon = join(pivot_mcu_data, mcu_lat_lon_data, type = "left")
+mcu_final_lat_lon$City = "MCU"
+
+#NCL data
 
 
-postcodes = c("PR3 0SG", "M45 6GN", "EX165BL")
+ncl_final_lat_lon = cbind(ncl_org_data, read.table(text = ncl_org_data$Location..WKT., sep = "("))
+ncl_final_lat_lon$V2 = as.character(ncl_final_lat_lon$V2)
+ncl_final_lat_lon = cbind(ncl_final_lat_lon, read.table(text = as.character(ncl_final_lat_lon$V2), sep = ")"))
+ncl_final_lat_lon = ncl_final_lat_lon[,-c(8,9,11)]
+ncl_final_lat_lon$V1 = as.character(ncl_final_lat_lon$V1)
 
-postcodes
+ncl_final_lat_lon = cbind(ncl_final_lat_lon, read.table(text = as.character(ncl_final_lat_lon$V1), sep = ""))
+ncl_final_lat_lon = ncl_final_lat_lon[,-c(8)]
 
-unique(mcu_agg_1$PostCodes)
+names(ncl_final_lat_lon)[8] = "Longitude"
+names(ncl_final_lat_lon)[9] = "Latitude"
 
-pc_list1 <- list(
-  postcodes = unique(mcu_agg_1$PostCodes))
-
-post1 = bulk_postcode_lookup(pc_list1)
-
+ncl_final_lat_lon$City = "NCL"
