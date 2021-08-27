3225105a0ef9a85e335a5caeafc9d3ff24781728
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2021-08-27 16:22:06 +0100

    Commented the codes to check each code based on the data and will change the file name

--- DataVisualization_Dissertation/munge/02-A.R
+++ DataVisualization_Dissertation/munge/02-A.R
@@ -1,17 +1,11 @@
 #Manchester data pre-processing step:
-names(MCU_data)[2] = "NO"
-names(MCU_data)[3] = "PM10"
-names(MCU_data)[4] = "NO2"
-names(MCU_data)[5] = "NOx"
-names(MCU_data)[8] = "O3"
-names(MCU_data)[9] = "SO2"
 
 #Sample comparision
 mcu_org_data = MCU_data[,c("timestamp", "NO2", "PM10", "StationName", "PostCodes")]
 
-mcu_org_data$PostCodes[mcu_org_data$PostCodes=="M1"]="M1, piccadilly gardens"
+#mcu_org_data$PostCodes[mcu_org_data$PostCodes=="M1"]="M1, piccadilly gardens"
 
-ncl_org_data = ncl_data %>% filter(Variable == c("NO2","PM10"))
+#ncl_org_data = ncl_data %>% filter(Variable == c("NO2","PM10"))
 
 #Extract date and hour from timestamp for MCU data
 #mcu_org_data$date = as.POSIXct(mcu_org_data$timestamp, format = "%d-%m-%Y")
@@ -31,7 +25,7 @@ ncl_org_data = ncl_org_data %>% drop_na()
 mcu_org_data = mcu_org_data %>% drop_na()
 
 #Use pivot longer to convert the column into rows in MCU data
-pivot_mcu_data = mcu_org_data %>% pivot_longer(cols = -c(timestamp, StationName, PostCodes, date, hour) , names_to = "Variable", values_to = "Value")
+pivot_mcu_data = mcu_org_data %>% pivot_longer(cols = -c(timestamp, PostCodes, date, hour) , names_to = "Variable", values_to = "Value")
 
 
 #-----------------------------------------------------------------------------------------------------------
--- DataVisualization_Dissertation/munge/04-A.R
+++ DataVisualization_Dissertation/munge/04-A.R
@@ -1,36 +1,36 @@
-#Splitting the datasets into NO2 and PM10
-
-ncl_final_lat_lon_no2 = ncl_final_lat_lon %>% filter(Variable == 'NO2')
-ncl_final_lat_lon_pm10 = ncl_final_lat_lon %>% filter(Variable == 'PM10')
-
-mcu_final_lat_lon_no2 = mcu_final_lat_lon %>% filter(Variable == 'NO2')
-mcu_final_lat_lon_pm10 = mcu_final_lat_lon %>% filter(Variable == 'PM10')
-
-
-#drop the columns:
-ncl_final_lat_lon_no2 = ncl_final_lat_lon_no2[,c(3,2,4,10)]
-ncl_final_lat_lon_pm10 = ncl_final_lat_lon_pm10[,c(3,2,4,10)]
-
-mcu_final_lat_lon_no2 = mcu_final_lat_lon_no2[,c(1,6,7,10)]
-mcu_final_lat_lon_pm10 = mcu_final_lat_lon_pm10[,c(1,6,7,10)]
-
-names(mcu_final_lat_lon_no2)[1] = "Timestamp"
-names(mcu_final_lat_lon_pm10)[1] = "Timestamp"
-
-#timestamp conversion:
-
-ncl_final_lat_lon_no2$datetime = as.POSIXct(ncl_final_lat_lon_no2$Timestamp, format = "%Y-%m-%d %H:%M:%S")
-ncl_final_lat_lon_pm10$datetime = as.POSIXct(ncl_final_lat_lon_pm10$Timestamp, format = "%Y-%m-%d %H:%M:%S")
-
-mcu_final_lat_lon_no2$datetime = as.POSIXct(mcu_final_lat_lon_no2$Timestamp, format = "%d-%m-%Y %H:%M")
-mcu_final_lat_lon_pm10$datetime = as.POSIXct(mcu_final_lat_lon_pm10$Timestamp, format = "%d-%m-%Y %H:%M")
-
-#Join both no2 and pm10 dataframe of mcu and ncl:
-ncl_mcu_no2 = rbind(ncl_final_lat_lon_no2, mcu_final_lat_lon_no2)
-ncl_mcu_pm10 = rbind(ncl_final_lat_lon_pm10, mcu_final_lat_lon_pm10)
-
-ncl_mcu_no2 = ncl_mcu_no2[order(ncl_mcu_no2$Variable, ncl_mcu_no2$datetime),]
-ncl_mcu_pm10 = ncl_mcu_pm10[order(ncl_mcu_pm10$Variable, ncl_mcu_pm10$datetime),]
-
-write.csv(ncl_mcu_no2, file = 'data/ncl_mcu_no2.csv', row.names = FALSE)
-write.csv(ncl_mcu_pm10, file = 'data/ncl_mcu_pm10.csv', row.names = FALSE)
+# #Splitting the datasets into NO2 and PM10
+# 
+# ncl_final_lat_lon_no2 = ncl_final_lat_lon %>% filter(Variable == 'NO2')
+# ncl_final_lat_lon_pm10 = ncl_final_lat_lon %>% filter(Variable == 'PM10')
+# 
+# mcu_final_lat_lon_no2 = mcu_final_lat_lon %>% filter(Variable == 'NO2')
+# mcu_final_lat_lon_pm10 = mcu_final_lat_lon %>% filter(Variable == 'PM10')
+# 
+# 
+# #drop the columns:
+# ncl_final_lat_lon_no2 = ncl_final_lat_lon_no2[,c(3,2,4,10)]
+# ncl_final_lat_lon_pm10 = ncl_final_lat_lon_pm10[,c(3,2,4,10)]
+# 
+# mcu_final_lat_lon_no2 = mcu_final_lat_lon_no2[,c(1,6,7,10)]
+# mcu_final_lat_lon_pm10 = mcu_final_lat_lon_pm10[,c(1,6,7,10)]
+# 
+# names(mcu_final_lat_lon_no2)[1] = "Timestamp"
+# names(mcu_final_lat_lon_pm10)[1] = "Timestamp"
+# 
+# #timestamp conversion:
+# 
+# ncl_final_lat_lon_no2$datetime = as.POSIXct(ncl_final_lat_lon_no2$Timestamp, format = "%Y-%m-%d %H:%M:%S")
+# ncl_final_lat_lon_pm10$datetime = as.POSIXct(ncl_final_lat_lon_pm10$Timestamp, format = "%Y-%m-%d %H:%M:%S")
+# 
+# mcu_final_lat_lon_no2$datetime = as.POSIXct(mcu_final_lat_lon_no2$Timestamp, format = "%d-%m-%Y %H:%M")
+# mcu_final_lat_lon_pm10$datetime = as.POSIXct(mcu_final_lat_lon_pm10$Timestamp, format = "%d-%m-%Y %H:%M")
+# 
+# #Join both no2 and pm10 dataframe of mcu and ncl:
+# ncl_mcu_no2 = rbind(ncl_final_lat_lon_no2, mcu_final_lat_lon_no2)
+# ncl_mcu_pm10 = rbind(ncl_final_lat_lon_pm10, mcu_final_lat_lon_pm10)
+# 
+# ncl_mcu_no2 = ncl_mcu_no2[order(ncl_mcu_no2$Variable, ncl_mcu_no2$datetime),]
+# ncl_mcu_pm10 = ncl_mcu_pm10[order(ncl_mcu_pm10$Variable, ncl_mcu_pm10$datetime),]
+# 
+# write.csv(ncl_mcu_no2, file = 'data/ncl_mcu_no2.csv', row.names = FALSE)
+# write.csv(ncl_mcu_pm10, file = 'data/ncl_mcu_pm10.csv', row.names = FALSE)
